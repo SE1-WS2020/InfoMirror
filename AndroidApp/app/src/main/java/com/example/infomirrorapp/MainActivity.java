af509e9ccbfcf8b50b11315fdb5832ac5240c8bc
ro0f3r <olli.seiffermann@outlook.de>
2021-02-14 18:20:27 +0100

    created start and registry views

--- AndroidApp/app/src/main/java/com/example/infomirrorapp/MainActivity.java
+++ AndroidApp/app/src/main/java/com/example/infomirrorapp/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.infomirrorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    // public static final String EXTRA_MESSAGE = "com.example.infomirrorapp.MESSAGE"
+    private Button signInButton;
+    private Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        registerButton = findViewById(R.id.singInButton);
+
+
+    }
+
+    public void startRegisterActivity(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        /*
+        intent.putExtra(EXTRA_MESSAGE, message;
+         */
+        startActivity(intent);
     }
 }
---
+++ AndroidApp/app/src/main/java/com/example/infomirrorapp/RegisterActivity.java
@@ -0,0 +1,24 @@
+package com.example.infomirrorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import org.json.JSONObject;
+
+public class RegisterActivity extends AppCompatActivity {
+    private Button registerButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+    public void registerButtonOnClick(View view) {
+        
+    }
+}
